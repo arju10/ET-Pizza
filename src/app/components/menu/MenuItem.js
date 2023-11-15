fe25680e1061709d6f4a7b1c5da1ce20829330dd
arju10 <arjuarju71@gmail.com>
2023-11-15 17:08:47 +0600

    create about us section header home page

--- src/app/components/menu/MenuItem.js
+++ src/app/components/menu/MenuItem.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from 'react'
 
 const MenuItem = () => {
-    return (
-        <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all hover:shadow-md hover:shadow-black/25">
-            <div className='text-center '>
-            <img src="/pizza1.png" alt="pizza" className='max-h-auto max-h-24 block mx-auto'/>
-            </div>
-            <h4 className='font-semibold my-4 text-xl'>Pepperoni Pizza</h4>
-            <p className='text-gray-500 text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-            <button className="bg-primary text-white rounded-full px-8 py-2 mt-4">Add to cart $15</button>
-        </div>
-    );
-};
+  return (
+    <div className='bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all hover:shadow-md hover:shadow-black/25'>
+      <div className='text-center '>
+        <img
+          src='/pizza1.png'
+          alt='pizza'
+          className='max-h-auto max-h-24 block mx-auto'
+        />
+      </div>
+      <h4 className='font-semibold my-4 text-xl'>Pepperoni Pizza</h4>
+      <p className='text-gray-500 text-sm'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      </p>
+      <button className='bg-primary text-white rounded-full px-8 py-2 mt-4'>
+        Add to cart $15
+      </button>
+    </div>
+  )
+}
 
-export default MenuItem;
+export default MenuItem
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from './components/layout/Header'
 import Hero from './components/layout/Hero'
 import HomeMenu from './components/layout/HomeMenu'
+import SectionHeaders from './components/layout/SectionHeaders'
 
 export default function Home () {
   return (
@@ -8,6 +9,24 @@ export default function Home () {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className='text-center my-16'>
+        <SectionHeaders subHeader={'Our Story'} mainHeader={'About Us'} />
+        <div className='text-gray-500 mx-w-2xl mx-w-md mt-4 flex flex-col gap-4'>
+          <p>
+            Welcome to our delicious world of pizzas! We take pride in crafting
+            the finest pizzas using the freshest ingredients to satisfy your
+            cravings. Our journey began with a passion for creating the perfect
+            pizza experience.
+          </p>
+          <p>
+            At our pizzeria, we believe in delivering not just a meal, but a
+            delightful experience. From classic Margherita to unique
+            specialities, each pizza is a masterpiece that reflects our
+            commitment to quality and taste. Join us on this flavorful
+            adventure!
+          </p>
+        </div>
+      </section>
     </>
   )
 }
